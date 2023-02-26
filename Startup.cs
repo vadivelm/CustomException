987e067c4ca84abaac86be0a6a8dd2ed93f8eab6
Sundaravadivel <sundaravadivel.m@outlook.com>
2023-02-26 18:51:30 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(CustomException.Startup))]
+
+namespace CustomException
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Register the custom exception handler
+            IConfigurationRoot config = BuildConfiguration();
+            ConfigureServices(builder.Services, config);
+            //builder.Services.AddWebJobsExceptionHandler<CustomExceptionHandler>();
+        }
+
+        private void ConfigureServices(IServiceCollection services, IConfigurationRoot config)
+        {
+            
+        }
+
+        private IConfigurationRoot BuildConfiguration()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddEnvironmentVariables().Build();
+            return config;
+        }
+    }
+}
